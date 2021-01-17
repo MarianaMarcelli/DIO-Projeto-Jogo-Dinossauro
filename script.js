efe3dc4a52db35959015773e9a317d5a3ef4f375
Mariana Marcelli <marimmarcelli@gmail.com>
2021-01-17 07:38:58 -0300

    Jogo Finalizado

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
     if(event.keyCode === 32) {
@@ -11,7 +12,7 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-    let position = 0;
+    
     isJumping = true;
 
     let upInterval = setInterval(() =>{
@@ -36,10 +37,28 @@ function jump() {
 function creatCactus() {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
 
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
+
+    let leftInterval = setInterval( () => {
+        if(cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }else if (cactusPosition >0 && cactusPosition < 60 && position <60){
+
+            clearInterval(leftInterval)
+            document.body.innerHTML = '<h1 class = "game-over"> Fim de Jogo</h1>';
+
+        }else {
+            cactusPosition -=10;
+            cactus.style.left = cactusPosition + 'px'
+        }    
+    }, 20);
+
+    setTimeout(creatCactus, randomTime);
 }
 
 creatCactus();
